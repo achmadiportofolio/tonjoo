9ef922d6490df66ca499bc793dec36c638c6a5f9
Bayu Hendra Winata <bayu@javan.co.id>
2018-12-30 18:52:17 +0700

    dashboard template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,29 +1,28 @@
-
 @extends('base')
 @section('content')
     <div class="row">
         <div class="col-lg-3">
             <ul class="list-group">
-                <li class="list-group-item" >
-                    <a href="{{url('/')}}" class="text-decoration-none" >
+                <li class="list-group-item">
+                    <a href="{{url('/')}}" class="text-decoration-none">
                         <h4>Home</h4>
                     </a>
                 </li>
                 <li class="list-group-item disabled" aria-disabled="true"><h4>Dashboard</h4></li>
                 <li class="list-group-item">
-                       <h5>Data Transaksi </h5>
+                    <h5>Data Transaksi </h5>
                 </li>
-                <li class="list-group-item">
-                   <a href="{{route('sewaKendaraan.create')}}" class="text-decoration-none" > Tambah Data Transaksi {{ Route::currentRouteName()=='sewaKendaraan.create'?'selected': '' }} </a>
+                <li class="list-group-item {{ Route::currentRouteName()=='sewaKendaraan.create'?'list-group-item-success': '' }} ">
+                    <a href="{{route('sewaKendaraan.create')}}" class="text-decoration-none"> Tambah Data Transaksi </a>
                 </li>
-                <li class="list-group-item">
-                   <a href="{{route('sewaKendaraan.index')}}" class="text-decoration-none" > List Data Transaksi {{ Route::currentRouteName()=='sewaKendaraan.index'?'selected': '' }} </a>
+                <li class="list-group-item {{ Route::currentRouteName()=='sewaKendaraan.index'?'list-group-item-success': '' }} ">
+                    <a href="{{route('sewaKendaraan.index')}}" class="text-decoration-none"> List Data Transaksi </a>
                 </li>
-                <li class="list-group-item">
-                   <a href="{{route('sewaKendaraan.index')}}" class="text-decoration-none" > Rekap Transaksi {{ Route::currentRouteName()=='sewaKendaraan.index'?'selected': '' }} </a>
+                <li class="list-group-item {{ Route::currentRouteName()=='sewaKendaraan.index'?'list-group-item-success': '' }} ">
+                    <a href="{{route('sewaKendaraan.index')}}" class="text-decoration-none"> Rekap Transaksi </a>
                 </li>
-                <li class="list-group-item" >
-                    <a href="{{url('/landing_page')}}" class="text-decoration-none" >
+                <li class="list-group-item {{ Route::currentRouteName()=='landing.page'?'list-group-item-success': '' }}">
+                    <a href="{{url('/landing_page')}}" class="text-decoration-none">
                         <h4>Layout</h4>
                     </a>
                 </li>
