74561570b88c429692dc5d95e54da3f332b0eeab
Achmadi <diahmad330@gmail.com>
2019-01-01 14:39:05 +0700

    typo

--- public/index.php
+++ public/index.php
@@ -53,7 +53,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
-exi('xxx');
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
